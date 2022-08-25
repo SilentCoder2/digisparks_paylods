23f3171ed7945e1a82de4f9e71bdf95fe625f450
Ankit Halder <112043813+SilentCoder2@users.noreply.github.com>
2022-08-25 08:38:22 +0530

    Update chorme.ino

--- unlimited_chrome_openner/chorme.ino
+++ unlimited_chrome_openner/chorme.ino
@@ -1,22 +1,24 @@
 #include <DigiKeyboard.h>
 
-#define link "https://google.com/"
+#define link "https://google.com/" // You can change the link
 
-void setup() {
-  
+void setup()
+{
+    // blank
 }
 
-void loop() {
-  
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  delay(250);    
-  DigiKeyboard.print(link);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  //delay(200);
+void loop()
+{
 
+    DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // send key window + r to start the rum command
+    delay(250);                                      /// wait for 250 mili second
+    DigiKeyboard.print(link);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER); // send enter key
+                                           // delay(200);
 
+    /* led blink after one cycle is completed*/
     digitalWrite(1, HIGH);
     delay(500);
     digitalWrite(1, LOW);
-   // delay(200);
+    // delay(200);
 }
